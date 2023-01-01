d067c9ef92246603782fe0cf7fc3da9fd81e953b
PrashantFiles1 <109992225+PrashantFiles1@users.noreply.github.com>
2023-01-02 02:06:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from configs import Config
 from pyrogram import Client, filters, idle
 from pyrogram.errors import QueryIdInvalid
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InlineQuery, InlineQueryResultArticle,InputTextMessageContent
-from LazyDeveloper.forcesub import ForceSub
 import asyncio
 
 # Bot Client for Inline Search
