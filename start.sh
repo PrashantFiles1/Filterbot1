3d41df58a5cf34a60376c10a92a18ade0a25cfd0
PrashantFiles1 <109992225+PrashantFiles1@users.noreply.github.com>
2023-01-02 01:25:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ echo "Cloning Repo...."
 git clone https://github.com/PrashantFiles1/PrashantLinksBOT
 cd /PrashantLinksBOT
 pip3 install -U -r requirements.txt
-echo "⚡️⚡️ Starting PrashantLinksBOT..."
+echo "⚡️⚡️ Starting Filterbot1..."
 python3 main.py
