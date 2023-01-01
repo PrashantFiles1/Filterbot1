d0e9be5a66a7376b5ac7c690f66ca2915e81c851
PrashantFiles1 <109992225+PrashantFiles1@users.noreply.github.com>
2023-01-02 03:26:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ async def help_handler(_, event: Message):
 async def inline_handlers(_, event: Message):
     if event.text == '/start':
         return
-    answers = f'**📂 Results ➠ {event.text} \n Wait_Uploading \n**'
+    answers = f'**📂 Results ➠ {event.text} \n Wait For Uploading... \n**'
     async for message in User.search_messages(chat_id=Config.CHANNEL_ID, limit=50, query=event.text):
         if message.text:
             thumb = None
